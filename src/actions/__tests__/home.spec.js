ac7dab5997e4175dd4bd413a498d2ed4f8205134
Kenny Inthirath <kenny.inthirath@cancerlinq.org>
2018-01-23 16:32:34 -0800

    Fix for Jest transpiling

--- src/actions/__tests__/home.spec.js
+++ src/actions/__tests__/home.spec.js
@@ -2,7 +2,7 @@ import { toggleHome, TOGGLE_HOME } from 'Actions/home'
 
 describe('home', () => {
   describe('toggleHome', () => {
-    it('should have return the correct type const', () => {
+    it('should have return the correct action TYPE', () => {
       expect(toggleHome().type).toBe(TOGGLE_HOME)
     })
   })
